a34b4be7bbbb10129c36c548790a39095ebe9ca5
FIRST_NAME LAST_NAME <1004ok04@gmail.com>
2020-12-06 18:59:22 +0900

    Create Custermer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Csutomer from './componente/Customer'
+import Customer from './componente/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} let="logo" />
-      <h2>Let's develop management syttgeml!</h2>
-      
-    </div>
-  );
+const customers = [
+{
+  'id':1,
+  'image':'http://placeimg.com/64/64/1',
+  'name': '김태홍',
+  'birthday':'961122',
+  'gender':'남자',
+  'job':'대학생' 
+},
+{
+  'id':2,
+  'image':'http://placeimg.com/64/64/2',
+  'name': '홍길동',
+  'birthday':'920419',
+  'gender':'남자',
+  'job':'프로그래머' 
+},
+{
+  'id':3,
+  'image':'http://placeimg.com/64/64/3',
+  'name': '가나다',
+  'birthday':'661233',
+  'gender':'여자',
+  'job':'대학생' 
+},
+]
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c =>{
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name = {c.name}
+                birthday ={c.birthday}
+                gender = {c.gender}
+                job = {c.job}
+                />
+            )
+          })
+
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
