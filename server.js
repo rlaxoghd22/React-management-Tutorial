abbc96eec0343f61b226dbc6d86688d00685a2a1
FIRST_NAME LAST_NAME <1004ok04@gmail.com>
2020-12-07 22:04:55 +0900

    Add Node.js Express Server Module

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/api/hello', (req, res) => {
+    res.send({message:'Hello Enxpress!'});
+});
+app.listen(port, () => console.log(`Listenin on port ${port}`));
